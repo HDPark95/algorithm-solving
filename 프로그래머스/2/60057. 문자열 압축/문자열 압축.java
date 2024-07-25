02f90bfd7751616ad5c1854b04cc763a2cca9183
HyundooPark <66261111+HDPark95@users.noreply.github.com>
2024-07-25 21:04:07 +0900

    [level 2] Title: 문자열 압축, Time: 6.47 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,47 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        if(s.length() == 1){
-            return 1;
-        }
-        int answer = Integer.MAX_VALUE;
-        for (int i = 1; i <= s.length()/2; i++) {  //절반넘어가면 할 필요없음 못줄임.
-            String compressedWord = compress(s, i);
-            answer = Math.min(answer, compressedWord.length());
-        }
+        int answer = 0;
         return answer;
-    }
-    
-    public String compress(String s, int length){
-        StringBuilder sb = new StringBuilder();
-        Queue<String> queue = new LinkedList<>();
-        for (int i = 0; i < s.length(); i += length) {
-            queue.add(s.substring(i, Math.min(i + length, s.length())));
-        }
-
-        String prev = queue.poll();
-        int count = 1;
-        while (!queue.isEmpty()) {
-            String current = queue.poll();
-            if (current.equals(prev)) {
-                count++;
-            } else {
-                if (count > 1) {
-                    sb.append(count).append(prev);
-                } else {
-                    sb.append(prev);
-                }
-                prev = current;
-                count = 1;
-            }
-        }
-
-        if (count > 1) {
-            sb.append(count).append(prev);
-        } else {
-            sb.append(prev);
-        }
-        return sb.toString();
     }
 }
