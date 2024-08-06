a292a72b9608c0892d8adb80c931fe6d82950cb0
HyundooPark <66261111+HDPark95@users.noreply.github.com>
2024-08-06 23:17:31 +0900

    [level 2] Title: N-Queen, Time: 159.89 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,34 +1,6 @@
 class Solution {
-    int N;
-    int[] visited; 
-    int answer = 0;
     public int solution(int n) {
-        N = n;
-        visited = new int[N];
-        dfs(0);
+        int answer = 0;
         return answer;
-    }
-    public void dfs(int checked){
-         if (checked == N){
-            answer++;
-            return;
-        }
-        for (int i = 0; i < N; i++) {
-            if (canCheck(checked, i)){
-                visited[checked] = i;
-                dfs(checked + 1);
-            }
-        }
-    }
-
-    public boolean canCheck(int row, int col){
-         for (int i = 0; i < row; i++) {
-            if(visited[i] == col){ //가로 확인
-                return false;
-            }else if(Math.abs(row - i) == Math.abs(col - visited[i])){ //대각선 확인
-                return false;
-            }
-        }
-        return true;
     }
 }
