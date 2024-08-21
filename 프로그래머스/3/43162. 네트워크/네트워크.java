0586b5d3d965a1588b47be737be8c1c3ca72ef42
HyundooPark <66261111+HDPark95@users.noreply.github.com>
2024-08-21 20:24:45 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public void dfs(int i, int[][] computers, boolean[] visited) {
+        visited[i] = true;
+        for (int j = 0; j < computers.length; j++) { 
+            if (computers[i][j] == 1 && !visited[j]) { 
+                dfs(j, computers, visited);
+            }
+        }
+    }
+}
