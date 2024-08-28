18fb2b2c45082d20c0b730838283bb2abaecd1e5
HyundooPark <66261111+HDPark95@users.noreply.github.com>
2024-08-29 00:19:44 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.94 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,36 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] cards) {
-        int n = cards.length;
-        boolean[] visited = new boolean[n];
-        List<Integer> groups = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                int groupSize = getGroupSize(cards, visited, i);
-                groups.add(groupSize);
-            }
-        }
-        
-        if (groups.size() == 1) {
-            return 0;
-        }
-        
-        Collections.sort(groups, Collections.reverseOrder());
-        return groups.get(0) * groups.get(1);
-    }
-    
-    private int getGroupSize(int[] cards, boolean[] visited, int start) {
-        int size = 0;
-        int current = start;
-        
-        while (!visited[current]) {
-            visited[current] = true;
-            size++;
-            current = cards[current] - 1;
-        }
-        
-        return size;
+        int answer = 0;
+        return answer;
     }
 }
